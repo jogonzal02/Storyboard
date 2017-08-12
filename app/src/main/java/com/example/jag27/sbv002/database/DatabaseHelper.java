4f0571efb0e9e51d8fa0dbf79653259cdd6ed847
John Gonzalez <jogonzal02@gmail.com>
2017-08-12 19:41:30 -0400

    -Created a Character table and a many to many table in database

--- app/src/main/java/com/example/jag27/sbv002/database/DatabaseHelper.java
+++ app/src/main/java/com/example/jag27/sbv002/database/DatabaseHelper.java
@@ -11,7 +11,8 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
     //Database info and version
     private static final String DATABASE_NAME = "storyboard.db";
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 6;
+
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -19,20 +20,24 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
         sqLiteDatabase.execSQL(CREATE_NOTE);
+        sqLiteDatabase.execSQL(CREATE_CHARACTER);
+        sqLiteDatabase.execSQL(CREATE_BRIDGE);
     }
+
+    private static final String DATABASE_ALTER_NOTE_1 = "ALTER TABLE " + Constants.NOTES_TABLE + " ADD COLUMN " +
+            Constants.COLUMN_SUBPLOT + " TEXT";
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
-        String sql = "ALTER TABLE " + Constants.NOTES_TABLE + " ADD COLUMN " +
-                Constants.COLUMN_SUBPLOT + " TEXT";
-        sqLiteDatabase.execSQL(sql);
-
-
+        if(i < 4) sqLiteDatabase.execSQL(DATABASE_ALTER_NOTE_1);
+        if(i < 5) sqLiteDatabase.execSQL(CREATE_CHARACTER);
+        if(i < 6) sqLiteDatabase.execSQL(CREATE_BRIDGE);
     }
 
-    //Creating table query
+    //Creating tables
     private static final String CREATE_NOTE = "create table "
             + Constants.NOTES_TABLE
             + "("
@@ -44,5 +49,20 @@ public class DatabaseHelper extends SQLiteOpenHelper{
             + Constants.COLUMN_POSITION + " integer not null, "
             + Constants.COLUMN_MODIFIED_TIME + " integer not null, "
             + Constants.COLUMN_CREATED_TIME + " integer not null " + ")";
+
+    private static final String CREATE_CHARACTER = "create table "
+            + Constants.CHARACTER_TABLE
+            + "("
+            + Constants.COLUMN_ID + " integer primary key autoincrement, "
+            + Constants.COLUMN_TITLE + " text not null, "
+            + Constants.COLUMN_CHARACTER + " text not null, "
+            + Constants.COLUMN_COLOR + " integer not null " + ")";
+
+    private static final String CREATE_BRIDGE = "create table "
+            + Constants.BRIDGE_TABLE
+            +"("
+            + Constants.COLUMN_ID + " integer primary key autoincrement, "
+            + Constants.COLUMN_NOTEID + " integer not null, "
+            + Constants.COLUMN_CHARACTERID + " integer not null " + ")";
 
 }
--- app/src/main/java/com/example/jag27/sbv002/utility/Constants.java
+++ app/src/main/java/com/example/jag27/sbv002/utility/Constants.java
@@ -5,8 +5,12 @@ package com.example.jag27.sbv002.utility;
 
 public class Constants {
     public  static final String NOTES_TABLE = "NOTES";
+    public static final String CHARACTER_TABLE = "CHARACTER";
+    public static final String BRIDGE_TABLE = "BRIDGE";
 
     public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NOTEID = "note_id";
+    public static final String COLUMN_CHARACTERID = "character_id";
     public static final String COLUMN_TITLE = "title";
     public static final String COLUMN_SUBTITLE = "subtitle";
     public static final String COLUMN_SUBPLOT = "subplot";
@@ -14,12 +18,9 @@ public class Constants {
     public static final String COLUMN_POSITION = "position";
     public static final String COLUMN_MODIFIED_TIME = "modified_time";
     public static final String COLUMN_CREATED_TIME = "created_time";
+    public static final String COLUMN_CHARACTER = "character";
+    public static final String COLUMN_COLOR = "color";
 
-//    public static final String[] COLUMNS = {
-//            Constants.COLUMN_ID,
-//            Constants.COLUMN_TITLE,
-//            Constants.COLUMN_SUBTITLE,
-//            Constants.COLUMN_CONTENT,
-//            Constants.COLUMN_CREATED_TIME
-//    };
+
+
 }
